3dd1e5d81acc0ae6a5133ab2596f9adbc68a8b62
cattlee <305851097@qq.com>
2016-05-31 15:35:53 +0800

    1、如果不是第一个页面，不能滑动出左侧菜单       要点：OnPageChangeListener事件要给TagPageIndicator来操作，原因tpi 把viewpager的OnPageChangeListene给取消掉了 2、新闻中心的页面  利用按钮进行切换不会越界  由于在viewpage中  有防止越界设定   if (item < 0) {             item = 0;         } else if (item >= mAdapter.getCount()) {             item = mAdapter.getCount() - 1;         }

--- src/com/itheima62/smartbj/newscenterpage/NewsBaseNewsCenterPage.java
+++ src/com/itheima62/smartbj/newscenterpage/NewsBaseNewsCenterPage.java
@@ -1,5 +1,3 @@
-
-
 package com.itheima62.smartbj.newscenterpage;
 
 import java.util.ArrayList;
@@ -7,6 +5,7 @@ import java.util.List;
 
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +15,7 @@ import com.itheima62.smartbj.R;
 import com.itheima62.smartbj.activity.MainActivity;
 import com.itheima62.smartbj.domain.NewsCenterData;
 import com.itheima62.smartbj.domain.NewsCenterData.NewsData.ViewTagData;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
 import com.lidroid.xutils.view.annotation.event.OnClick;
@@ -29,28 +29,74 @@ import com.viewpagerindicator.TabPageIndicator;
  *     @ svn提交者：$Author: gd $ @ 提交时间: $Date: 2015-07-06 14:57:36 +0800 (Mon, 06
  *     Jul 2015) $ @ 当前版本: $Rev: 33 $
  */
-public class NewsBaseNewsCenterPage extends BaseNewsCenterPage
-{
+public class NewsBaseNewsCenterPage extends BaseNewsCenterPage {
 
 	@ViewInject(R.id.newcenter_vp)
-	private ViewPager			vp_newscenter;
+	private ViewPager vp_newscenter;
 
 	@ViewInject(R.id.newcenter_tpi)
-	private TabPageIndicator	tpi_newscenter;
-	//利用注入来完成事件的添加
+	private TabPageIndicator tpi_newscenter;
+
+	// 利用注入来完成事件的添加
 	@OnClick(R.id.newscenter_id_nextpage)
-	public void next(View v){
-		//切换到下一个页面，利用viewPage来实现呢
-		vp_newscenter.setCurrentItem(vp_newscenter.getCurrentItem()+1);
+	public void next(View v) {
+		// 切换到下一个页面，利用viewPage来实现呢
+		vp_newscenter.setCurrentItem(vp_newscenter.getCurrentItem() + 1);
 	}
 
-	private List<ViewTagData>	viewTagDatas	= new ArrayList<NewsCenterData.NewsData.ViewTagData>(); // 页签的数据
+	private List<ViewTagData> viewTagDatas = new ArrayList<NewsCenterData.NewsData.ViewTagData>(); // 页签的数据
 
 	public NewsBaseNewsCenterPage(MainActivity mainActivity,
 			List<ViewTagData> children) {
 		super(mainActivity);
 		// TODO Auto-generated constructor stub
 		this.viewTagDatas = children;
+	}
+
+	@Override
+	public void initEvent() {
+		// 添加自己的事件
+
+		// 给viewpage添加页面切换的监听事件，当页面位于第一个，可以滑动出左侧菜单，否则不滑动
+		//slidingmenu开源框架中  TabPageIndicator的效果更直接故直接选择vp_newscenter进行设置 而不是ViewPager
+		tpi_newscenter.setOnPageChangeListener(new OnPageChangeListener() {
+
+			/*
+			 * (non-Javadoc) 此方法用于监控页面停留的位置
+			 * 
+			 * @see
+			 * android.support.v4.view.ViewPager.OnPageChangeListener#onPageSelected
+			 * (int)
+			 */
+			@Override
+			public void onPageSelected(int position) {
+				// 利用此方法实现 当页面位于第一个可以画出左侧菜单
+				if (position == 0) {
+					// 位于第一个，可以滑动到左侧菜单    setTouchModeAbove()方法设置是否可以滑动出菜单
+					mainActivity.getSlidingMenu().setTouchModeAbove(
+							SlidingMenu.TOUCHMODE_FULLSCREEN);
+				} else {
+					// 不可以滑动到左侧菜单
+					mainActivity.getSlidingMenu().setTouchModeAbove(
+							SlidingMenu.TOUCHMODE_NONE);
+				}
+
+			}
+
+			@Override
+			public void onPageScrolled(int position, float positionOffset,
+					int positionOffsetpixels) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+		super.initEvent();
 	}
 
 	@Override
@@ -81,10 +127,10 @@ public class NewsBaseNewsCenterPage extends BaseNewsCenterPage
 	 * @创建时间 2015-7-6 下午4:29:02
 	 * @描述 页签对应ViewPage的适配器
 	 * 
-	 *     @ svn提交者：$Author: gd $ @ 提交时间: $Date: 2015-07-06 16:59:32 +0800 (Mon, 06 Jul 2015) $ @ 当前版本: $Rev: 33 $
+	 *     @ svn提交者：$Author: gd $ @ 提交时间: $Date: 2015-07-06 16:59:32 +0800 (Mon,
+	 *     06 Jul 2015) $ @ 当前版本: $Rev: 33 $
 	 */
-	private class MyAdapter extends PagerAdapter
-	{
+	private class MyAdapter extends PagerAdapter {
 
 		@Override
 		public int getCount() {
@@ -97,7 +143,7 @@ public class NewsBaseNewsCenterPage extends BaseNewsCenterPage
 		 */
 		@Override
 		public CharSequence getPageTitle(int position) {
-			//获取页签的数据
+			// 获取页签的数据
 			return viewTagDatas.get(position).title;
 		}
 
