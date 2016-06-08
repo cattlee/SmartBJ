191a11d22209299575bc6baf6eebc5aef4f4d2d6
cattlee <305851097@qq.com>
2016-06-08 11:30:44 +0800

    左侧菜单   组图   的界面显示

--- src/com/itheima62/smartbj/utils/MyConstants.java
+++ src/com/itheima62/smartbj/utils/MyConstants.java
@@ -1,19 +1,21 @@
 package com.itheima62.smartbj.utils;
 
 /**
- * @author Administrator
- * @创建时间 2015-7-4 上午10:42:26
- * @描述 TODO
- * 
- *     @ svn提交者：$Author: gd $ @ 提交时间: $Date: 2015-07-06 11:15:56 +0800 (Mon, 06
- *     Jul 2015) $ @ 当前版本: $Rev: 25 $
+ * @author ltf
+ * @创建时间2016-6-8上午9:51:19
+ * @工程名SmartBJ
+ * @描述              常用 属性   属性有顺序
+ * @svn提交者：$Auther$
+ * @提交时间：${date}${time}
+ * @当前版本：$Rev$
  */
-public interface MyConstants {
-	// apk发布修改该ip ip 或者 域名 www.henhao.com/zhbj/categories.json
-	String NEWSCENTERURL = "http://10.0.2.2:8080/zhbj/categories.json";
-	String SERVERURL = "http://10.0.2.2:8080/zhbj";
-	String CONFIGFILE = "cachevalue";// sp的文件名
-	String ISSETUP = "issetup";// 是否设置向导界面设置过数据
-	String READNEWSIDS="readnewsids";//保存 读过的新闻id
-
+public interface MyConstants 
+{
+	//apk发布修改该ip ip 或者 域名 www.henhao.com/zhbj/categories.json
+	String SERVERURL = "http://10.0.2.2:8080/zhbj";//服务的url
+	String NEWSCENTERURL = SERVERURL + "/categories.json";//新闻中心的url
+	String PHOTOSURL = SERVERURL + "/photos/photos_1.json";//组图的url
+	String CONFIGFILE = "cachevalue";//sp的文件名
+	String ISSETUP = "issetup";//是否设置向导界面设置过数据
+	String READNEWSIDS = "readnewsids";//保存    读过的新闻id
 }
